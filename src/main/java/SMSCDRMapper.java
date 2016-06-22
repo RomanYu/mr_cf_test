904ef6c59b302722672847f4215c9aaf159f5960
yuyao <yuyao@yuyaodeMacBook-Pro.local>
2016-06-22 20:07:28 +0800

    add all

---
+++ src/main/java/SMSCDRMapper.java
@@ -0,0 +1,34 @@
+/**
+ * Created by yuyao on 16/6/22.
+ */
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mrunit.mapreduce.MapDriver;
+import org.apache.hadoop.mrunit.mapreduce.MapReduceDriver;
+import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SMSCDRMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    private Text status = new Text();
+    private final static IntWritable addOne = new IntWritable(1);
+
+    /**
+        * Returns the SMS status code and its count
+        */
+    protected void map(LongWritable key, Text value, Context context)
+            throws java.io.IOException, InterruptedException {
+
+        //655209;1;796764372490213;804422938115889;6 is the Sample record format
+        String[] line = value.toString().split(";");
+        // If record is of SMS CDR
+        if (Integer.parseInt(line[1]) == 1) {
+            status.set(line[4]);
+            context.write(status, addOne);
+        }
+    }
+}
